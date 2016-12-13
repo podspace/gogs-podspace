be6293f5e5de733afecb345d33c3b2119871b027
Gorgan <gorganrazvan123@gmail.com>
2016-12-13 12:36:13 +0200

    Changed ENV VAR placement

--- gogs-podspace.sh
+++ gogs-podspace.sh
@@ -5,11 +5,11 @@ RUN_USER="git"
 LOG_ROOT_PATH="/data/log"
 DB_PATH="/data/db/gogs.db"
 GOGS="/opt/gogs/gogs"
+APP_URL="http://gogs.apps.podspace.hu"
 
 : ${ADMIN_USERNAME:="gogs"}
 : ${ADMIN_PASSWORD:="admin"}
-: ${ADMIN_EMAIL:="admin@" }
-: ${APP_URL:="http://gogs.apps.podspace.hu"}
+: ${ADMIN_EMAIL:="admin@"}
 
 mkdir -p /data/repositories
 mkdir -p /data/log
@@ -28,7 +28,7 @@ REDIRECT=$(curl -I -X GET http://localhost:3000/ 2>/dev/null|grep 'Location:'|cu
 if [ "$REDIRECT" == "/install" ]; then
   # TODO Test for the HTTP status code of the install call, and fail if it was not successfull
   echo "[PODSPACE] Executing installation..."
-  curl 'http://localhost:3000/install' -H 'Content-Type: application/x-www-form-urlencoded' --data 'db_type=SQLite3&db_host=127.0.0.1%3A3306&db_user=root&db_passwd=&db_name=gogs&ssl_mode=disable&app_name=Gogs%3A+Go+Git+Service&domain=localhost&ssh_port=22&http_port=&smtp_host=&smtp_from=&smtp_email=&smtp_passwd=&enable_captcha=on&admin_name=&admin_passwd=&admin_confirm_passwd=&admin_email=' --data-urlencode run_user=${RUN_USER} --data-urlencode repo_root_path=${REPO_ROOT} --data-urlencode log_root_path=${LOG_ROOT_PATH} --data-urlencode db_path=${DB_PATH} --data-urlencode app_url=${APP_URL}
+  curl 'http://localhost:3000/install' -H 'Content-Type: application/x-www-form-urlencoded' --data 'db_type=SQLite3&db_host=127.0.0.1%3A3306&db_user=root&db_passwd=&db_name=gogs&ssl_mode=disable&app_name=Gogs%3A+Go+Git+Service&ssh_port=22&http_port=&smtp_host=&smtp_from=&smtp_email=&smtp_passwd=&enable_captcha=on&admin_name=&admin_passwd=&admin_confirm_passwd=&admin_email=' --data-urlencode run_user=${RUN_USER} --data-urlencode repo_root_path=${REPO_ROOT} --data-urlencode log_root_path=${LOG_ROOT_PATH} --data-urlencode db_path=${DB_PATH} --data-urlencode app_url=${APP_URL} --data-urlencode domain=${APP_URL}
   echo "[PODSPACE] Creating the admin user..."
   $GOGS admin create-user --name ${ADMIN_USERNAME} --password ${ADMIN_PASSWORD} --email ${ADMIN_EMAIL} --admin || (echo "[PODSPACE] Failed to create admin user." && exit 1)
 fi
